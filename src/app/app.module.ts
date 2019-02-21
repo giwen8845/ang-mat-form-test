bf6ada7070ea0de3e62cc5a55fbac2dd0a24f26b
havasi.balint <havasi.balint@tigra.hu>
2019-02-21 17:30:40 +0100

    Form első verzió service validáció nélkül

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MAT_DATE_LOCALE,
+  MatButtonModule,
+  MatCardModule, MatDatepickerModule,
+  MatGridListModule,
+  MatInputModule, MatNativeDateModule
+} from "@angular/material";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +21,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatDatepickerModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'hu'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
